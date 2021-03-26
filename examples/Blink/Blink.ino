fd68d2fec82b56a1ef2c4dbee806bf4d42b4e516
georgychen <georgychen@yahoo.com>
2021-03-26 16:10:33 +0800

    library re-structure

--- examples/Blink/Blink.ino
+++ examples/Blink/Blink.ino
@@ -1,16 +1,15 @@
-/*
 
+/*
 Blink Example to test platformio, github and library dependency
 
-*** PlatformIO ***
-Copy and paste this section of configuration code into platform.ini:
-[env:nodemcuv2]
+ PlatformIO 
+Copy and paste this section of configuration code into platform.ini
+[envnodemcuv2]
 platform = espressif8266
 board = nodemcuv2
 framework = arduino
 monitor_speed = 115200
 monitor_filters = send_on_enter
-
 */
 
 #include "Weight_Estimator.h"
